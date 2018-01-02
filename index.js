fe38554643db50457c16a14590689032a6f98b5f
Emmanuel Chappat <m4nu@hotmail.fr>
2018-01-02 11:35:52 +0100

    Soften jsx-a11y/no-noninteractive-element-interactions

--- index.js
+++ index.js
@@ -2,67 +2,85 @@ const airbnb = require('eslint-config-airbnb');
 const deepAssign = require('deep-assign');
 
 module.exports = deepAssign(airbnb,
-  {
-    "rules": {
-      "indent": [
-        "error",
-        4
-      ],
-      "react/jsx-indent": [
-        "error",
-        4
-      ],
-      "react/no-unused-prop-types": false,
+  const airbnb = require('eslint-config-airbnb');
+  const deepAssign = require('deep-assign');
 
-      "react/jsx-indent-props": [
-        "error",
-        4
-      ],
-      "react/jsx-filename-extension": [
-        "error",
-        {
-          "extensions": [
-            ".js",
-            ".jsx"
-          ]
-        }
-      ],
-      "no-underscore-dangle": 0,
-      "max-len": [
-        "error", 120, 4,
-        {
-          "ignoreUrls": true,
-          "ignoreComments": false,
-          "ignoreRegExpLiterals": true,
-          "ignoreStrings": true,
-          "ignoreTemplateLiterals": true,
-        }
-      ],
-      "space-unary-ops": 0,
-      "react/forbid-prop-types": 0,
-      "no-unused-vars": [
-        2,
-        {
-          "vars": "all",
-          "args": "none",
-          "ignoreRestSiblings": true
-        }
-      ]
-    },
-    "globals": {
-      "PIXI": true
-    },
-    "env": {
-      "browser": true,
-      "jest": true,
-      "jquery": true
-    },
-    "settings": {
-      "import/resolver": {
-        "webpack": {
-          "config": "./webpack.common.js"
+  module.exports = deepAssign(airbnb,
+    {
+      "rules": {
+        "indent": [
+          "error",
+          4
+        ],
+
+       "jsx-a11y/no-noninteractive-element-interactions": [
+           "warn",
+           {
+              "handlers": [
+                "onClick",
+                "onMouseDown",
+                "onMouseUp",
+                "onKeyPress",
+                "onKeyDown",
+                "onKeyUp",
+              ],
+        },
+       ],
+        "react/no-unused-prop-types": false,
+        "react/jsx-indent": [
+          "error",
+          4
+        ],
+        "react/jsx-indent-props": [
+          "error",
+          4
+        ],
+        "react/jsx-filename-extension": [
+          "error",
+          {
+            "extensions": [
+              ".js",
+              ".jsx"
+            ]
+          }
+        ],
+        "no-underscore-dangle": 0,
+        "max-len": [
+          "error", 120, 4,
+          {
+            "ignoreUrls": true,
+            "ignoreComments": false,
+            "ignoreRegExpLiterals": true,
+            "ignoreStrings": true,
+            "ignoreTemplateLiterals": true,
+          }
+        ],
+        "space-unary-ops": 1,
+        "react/forbid-prop-types": 0,
+        "no-unused-vars": [
+          2,
+          {
+            "vars": "all",
+            "args": "none",
+            "ignoreRestSiblings": true
+          }
+        ]
+      },
+      "globals": {
+        "PIXI": true
+      },
+      "env": {
+        "browser": true,
+        "jest": true,
+        "jquery": true
+      },
+      "settings": {
+        "import/resolver": {
+          "webpack": {
+            "config": "./webpack.common.js"
+          }
         }
       }
     }
-  }
+  )
 )
